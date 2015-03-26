ae2e3d2ff6f6561564dc03d0fd09b5ce0a976fa0
ildyria <beviguier@gmail.com>
2015-03-26 16:44:29 +0100

    you need to use arguments to make it work to keep the spirit

--- Indexator/src/index/IndexFiles.java
+++ Indexator/src/index/IndexFiles.java
@@ -1,3 +1,5 @@
+package index;
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements. See the NOTICE file distributed with
@@ -50,23 +52,39 @@ public class IndexFiles {
 	private IndexFiles() {
 	}
 
-	/** Index all text files under a directory. */
-	public static void main(String[] args) {
-		String usage = "java org.apache.lucene.demo.IndexFiles"
-				+ " [-index INDEX_PATH] [-docs DOCS_PATH] [-update]\n\n"
-				+ "This indexes the documents in DOCS_PATH, creating a Lucene index"
-				+ "in INDEX_PATH that can be searched with SearchFiles";
-		String indexPath = "resources/index";
-		String docsPath = "resources/";
-		boolean create = true;
-		final Path docDir = Paths.get(docsPath);
-		if (!Files.isReadable(docDir)) {
-			System.out
-					.println("Document directory '"
-							+ docDir.toAbsolutePath()
-							+ "' does not exist or is not readable, please check the path");
-			System.exit(1);
-		}
+
+	  /** Index all text files under a directory. */
+	  public static void main(String[] args) {
+	    String usage = "java org.apache.lucene.demo.IndexFiles"
+	                 + " [-index INDEX_PATH] [-docs DOCS_PATH] [-update]\n\n"
+	                 + "This indexes the documents in DOCS_PATH, creating a Lucene index"
+	                 + "in INDEX_PATH that can be searched with SearchFiles";
+	    String indexPath = "index";
+	    String docsPath = null;
+	    boolean create = true;
+	    for(int i=0;i<args.length;i++) {
+	      if ("-index".equals(args[i])) {
+	        indexPath = args[i+1];
+	        i++;
+	      } else if ("-docs".equals(args[i])) {
+	        docsPath = args[i+1];
+	        i++;
+	      } else if ("-update".equals(args[i])) {
+	        create = false;
+	      }
+	    }
+
+	    if (docsPath == null) {
+	      System.err.println("Usage: " + usage);
+	      System.exit(1);
+	    }
+
+	    final Path docDir = Paths.get(docsPath);
+	    if (!Files.isReadable(docDir)) {
+	      System.out.println("Document directory '" +docDir.toAbsolutePath()+ "' does not exist or is not readable, please check the path");
+	      System.exit(1);
+	    }
+	    
 		Date start = new Date();
 		try {
 			System.out.println("Indexing to directory '" + indexPath + "'...");
