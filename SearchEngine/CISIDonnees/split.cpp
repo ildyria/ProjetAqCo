6a493269676e853a4579de1d4d9aed0da9e27ab0
ildyria <beviguier@gmail.com>
2015-03-31 14:27:01 +0200

    helloSplit

--- SearchEngine/CISIDonnees/split.cpp
+++ SearchEngine/CISIDonnees/split.cpp
@@ -6,27 +6,38 @@
 int main(int argc, char const *argv[])
 {
 	std::string str_line;
+	std::string str_num;
 	std::ifstream myfile;
 	std::ofstream tofiles;
 
 	std::string filename = "NONE";
 	myfile.open("CISI.ALLnettoye");
+	int number = 1;
+	int previous = 0;
 	while(!myfile.eof()) // To get you all the lines.
 	{
 		getline(myfile,str_line); // Saves the line in STRING.
 		if(str_line.compare(0,3,".I ") == 0)
 		{
+			str_num = str_line.substr(3,std::string::npos);
+			number = std::stoi(str_num,nullptr,0);
 			if(filename != "NONE")
 			{
 				tofiles.close();
 			}
 			getline(myfile,str_line); // Saves the line in STRING.
-			filename = "files/";
+
 			str_line.resize(str_line.size() -1);
-			filename = filename + str_line + ".txt";
-			std::cout << str_line << std::endl;
+			str_num.resize(str_num.size() -1);
+			filename = "files/[" + str_num + "] " + str_line + ".txt";
+			if(number != (previous + 1))
+			{
+				std::cout << "problem : " << number << std::endl;
+			}
+			previous = number;
 			tofiles.open(filename.c_str());
 			tofiles << str_line << std::endl;
+			number++;
 		}
 		else
 		{
