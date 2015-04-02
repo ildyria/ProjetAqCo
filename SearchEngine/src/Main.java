eb3484d2ac664e38ce2c995352fbcfa329fbc8c7
Guillaume Biez <guillaume.biez@insa-rennes.fr>
2015-04-02 21:11:05 +0200

    debut console

--- SearchEngine/src/Main.java
+++ SearchEngine/src/Main.java
@@ -1,9 +1,13 @@
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.custom.CustomAnalyzer;
+import org.apache.lucene.analysis.custom.CustomAnalyzer.Builder;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.util.TokenizerFactory;
 import org.apache.lucene.search.ScoreDoc;
@@ -17,48 +21,75 @@ import Indexing.SearchFiles;
 public class Main {
 
 	public static void main(String[] args) throws Exception{
-		//Choisir l'implémentation d'analyzer voulue ici
-		//TODO : faire un choix en commande à la limite ? pour vendredi (démo et rendu code)
-//Analyzer analyzerImpl = new StandardAnalyzer();
 		
-//		Analyzer analyzerImpl = CustomAnalyzer.builder(Paths.get("CISIDonnees"))
-//				   .withTokenizer("classic")
-//				   .addTokenFilter("standard")
-//				   .addTokenFilter("lowercase")
-//				   .addTokenFilter("stop", "ignoreCase", "false", "words", "motsvides.txt", "format", "wordset")
-//				   .build();
-		Analyzer analyzerImpl = CustomAnalyzer.builder(Paths.get("CISIDonnees"))
-				   .withTokenizer("standard")
-				   .addTokenFilter("standard")
-				   .addTokenFilter("lowercase")
-				   .addTokenFilter("stop", "ignoreCase", "false", "words", "motsvides.txt", "format", "wordset")
-				   .addTokenFilter("SnowBallPorter","language", "English")
-				   .addTokenFilter("removeduplicates")
-				   .build();
+		System.out.println("Bonjour");
+		Thread.sleep(200);
+		System.out.println("Bienvenue dans le Super Moteur de Recherche");
+		Thread.sleep(200);
+		System.out.println("Nous vous attendions !");
+		Thread.sleep(200);
+		System.out.println("Plusieurs choix vont s'offrir à vous, choisissez bien !");
+		Thread.sleep(200);
+		
+		String tokenizer = "standard";
+		List<List<String>> filters = new ArrayList<List<String>>();
+			//Analyzer analyzerImpl = new StandardAnalyzer();
+			/*Analyzer analyzerImpl = CustomAnalyzer.builder(Paths.get("CISIDonnees"))
+					   .withTokenizer("standard")
+					   .addTokenFilter("standard")
+					   .addTokenFilter("lowercase")
+					   .addTokenFilter("stop", "ignoreCase", "false", "words", "motsvides.txt", "format", "wordset")
+					   .addTokenFilter("SnowBallPorter","language", "English")
+					   .addTokenFilter("removeduplicates")
+					   .build();*/
+			
+			//new builder
+			Builder builder = CustomAnalyzer.builder(Paths.get("CISIDonnees"));
+			System.out.println("Le tokenizer actuel est : " + tokenizer);
+			System.out.println("Voulez vous le changer ? (y/n)");
+			Scanner sc = new Scanner(System.in);
+			String val = sc.nextLine();
+			while(!val.equals("y") && !val.equals("n"))
+			{
+				System.out.println("essayez encore !");
+				val = sc.nextLine();
+			}
+			if(val.equals("y")){
+				System.out.println("Les tokenizers disponibles sont :");
+				System.out.println(TokenizerFactory.availableTokenizers());
+				System.out.println("Entrez celui que vous voulez");
+				tokenizer = sc.nextLine();
+			}
+			sc.close();
+			
+			
+			
+			builder.withTokenizer(tokenizer);
+			Analyzer analyzerImpl = builder.build();
+			
+			//Fin de construction de l'analyzer
+			//Indexing
+			IndexFiles indexing = new IndexFiles(analyzerImpl);
+			indexing.run();
+			//Querying
+			SearchFiles searching = new SearchFiles(analyzerImpl);
+			Map<Integer,String> queries = searching.getAllQueries();
+	
+			System.out.println("thre ; prcs ; rcll ; speci ; error ; false");
+			
+			for(int i = 20; i < 30; ++i)
+			{
+				Map<Integer, TopDocs> queryResults = searching.executeAllQueries(queries);
+				Map<Integer, List<ScoreDoc>> queryFilteredResults = searching.applyThreshold(queryResults, (float)(i/100.));
 				
-		
-		IndexFiles indexing = new IndexFiles(analyzerImpl);
-		indexing.run();
-		
-		SearchFiles searching = new SearchFiles(analyzerImpl);
-		Map<Integer,String> queries = searching.getAllQueries();
-//		System.out.println("Taille queries" + queries.size());
-
-		System.out.println("thre ; prcs ; rcll ; speci ; error ; false");
-		for(int i = 20; i < 30; ++i)
-		{
-			Map<Integer, TopDocs> queryResults = searching.executeAllQueries(queries);
-//			searching.displayResults(queryResults);
-//			System.out.println("-----------");
-//			System.out.println("-----------");
-//			System.out.println("-----------");
-			Map<Integer, List<ScoreDoc>> queryFilteredResults = searching.applyThreshold(queryResults, (float)(i/100.));
-//			searching.displayFilteredResults(queryFilteredResults);
-			Benchmark bench = new Benchmark(queryFilteredResults);
-//			System.out.println("Total results : ");
-			System.out.println(String.format("%3.2g",(i/100.)) + " ; " + bench.toString());
-		}
-		System.out.println("thre ; prcs ; rcll ; speci ; error ; false");
-		System.out.println(TokenizerFactory.availableTokenizers());
+	//			searching.displayResults(queryResults);
+	//			searching.displayFilteredResults(queryFilteredResults);
+				
+				Benchmark bench = new Benchmark(queryFilteredResults);
+				
+	//			System.out.println("Total results : ");
+				System.out.println(String.format("%3.2g",(i/100.)) + " ; " + bench.toString());
+			}
+			System.out.println("thre ; prcs ; rcll ; speci ; error ; false");		
 	}
 }
